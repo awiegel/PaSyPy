5b1d75b40135f33763715c130323a1bbac4fcf7a
Alexander Wiegel <alexander.wiegel@rwth-aachen.de>
2021-05-22 17:49:02 +0200

    feat: hyperplane

--- pasypy/visualize.py
+++ pasypy/visualize.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import svm
 
 from variables import *
 
@@ -24,11 +26,47 @@ def draw_red_area():
                  r[1][0], r[1][0], r[1][1], r[1][1], r[1][0]], color='red')
 
 
+def draw_hyperplane():
+    X = []
+    Y = []
+    for i in G:
+        X.append([i[0][0], i[1][0]])
+        X.append([i[0][1], i[1][1]])
+        Y.append(0)
+        Y.append(0)
+
+    for i in R:
+        X.append([i[0][0], i[1][0]])
+        X.append([i[0][1], i[1][1]])
+        Y.append(1)
+        Y.append(1)
+
+    if 0 in Y and 1 in Y:
+        clf = svm.SVC(kernel='rbf', C=1000)
+        clf.fit(X, Y)
+        ax = plt.gca()
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        xx = np.linspace(xlim[0], xlim[1], 30)
+        yy = np.linspace(ylim[0], ylim[1], 30)
+        YY, XX = np.meshgrid(yy, xx)
+        xy = np.vstack([XX.ravel(), YY.ravel()]).T
+        Z = clf.decision_function(xy).reshape(XX.shape)
+        # plot decision boundary and margins
+        # plt instead of ax
+        ax.contour(XX, YY, Z, colors='b', levels=[-1, 0, 1], alpha=0.5,
+                   linestyles=['--', '-', '--'])
+        # plot support vectors
+        # ax.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=100,
+        # linewidth=1, facecolors='none', edgecolors='k')
+
+
 # Complete visualization part
 def generate_graph():
     init_graph()
     draw_green_area()
     draw_red_area()
+    draw_hyperplane()
 
 
 def show_graph():
