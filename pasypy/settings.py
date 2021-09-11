a2c5ecbb1c40acd29f11623e89f66059191b52a1
Alexander Wiegel <alexander.wiegel@outlook.com>
2021-09-11 15:31:25 +0200

    feat: add options for pre-sampling and sampling

--- pasypy/settings.py
+++ pasypy/settings.py
@@ -6,9 +6,11 @@ import webbrowser
 from pasypy import color
 from pasypy.color import Color
 
+pre_sampling = True # pylint: disable=C0103 # is not a constant
+sampling = True # pylint: disable=C0103 # is not a constant
 hyperplane = False # pylint: disable=C0103 # is not a constant
-colorblind_mode = False # pylint: disable=C0103 # is not a constant
 white_boxes = False # pylint: disable=C0103 # is not a constant
+colorblind_mode = False # pylint: disable=C0103 # is not a constant
 hatch_pattern = False # pylint: disable=C0103 # is not a constant
 skip_visualization = False # pylint: disable=C0103 # is not a constant
 
@@ -22,12 +24,16 @@ class Settings(tk.Frame):
         self.parent = parent
         self.window = None
         self.settings_frame = None
+        self.pre_sampling = None
+        self.pre_sampling_option = None
+        self.sampling = None
+        self.sampling_option = None
         self.hyperplane = None
         self.hyperplane_option = None
-        self.hatch_pattern = None
-        self.hatch_pattern_option = None
         self.white_boxes = None
         self.white_boxes_option = None
+        self.hatch_pattern = None
+        self.hatch_pattern_option = None
         self.colorblind_mode = None
         self.colorblind_option = None
         self.skip_visualization = None
@@ -51,52 +57,67 @@ class Settings(tk.Frame):
             self.settings_frame = tk.Frame(master=self.window, background='black')
             self.settings_frame.grid(row=0, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
 
+            self.pre_sampling = tk.BooleanVar()
+            self.pre_sampling_option = tk.Checkbutton(self.settings_frame, text='Pre-Sampling',variable=self.pre_sampling, onvalue=True, offvalue=False,
+                                                            command=self.set_pre_sampling_option, font=('',10), bg='white', fg='black', anchor=tk.W)
+            self.pre_sampling_option.grid(row=0, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            if pre_sampling:
+                self.pre_sampling_option.select()
+
+            self.sampling = tk.BooleanVar()
+            self.sampling_option = tk.Checkbutton(self.settings_frame, text='Sampling',variable=self.sampling, onvalue=True, offvalue=False,
+                                                            command=self.set_sampling_option, font=('',10), bg='white', fg='black', anchor=tk.W)
+            self.sampling_option.grid(row=1, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            if sampling:
+                self.sampling_option.select()
+
             self.hyperplane = tk.BooleanVar()
             self.hyperplane_option = tk.Checkbutton(self.settings_frame, text='Hyperplane',variable=self.hyperplane, onvalue=True, offvalue=False,
                                                     command=self.set_hyperplane_option, font=('',10), bg='white', fg='black', anchor=tk.W)
-            self.hyperplane_option.grid(row=0, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            self.hyperplane_option.grid(row=2, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
             if hyperplane:
                 self.hyperplane_option.select()
-
-            self.hatch_pattern = tk.BooleanVar()
-            self.hatch_pattern_option = tk.Checkbutton(self.settings_frame, text='Hatch Pattern',variable=self.hatch_pattern, onvalue=True, offvalue=False,
-                                                       command=self.set_hatch_pattern_option, font=('',10), bg='white', fg='black', anchor=tk.W)
-            self.hatch_pattern_option.grid(row=1, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
-            if hatch_pattern:
-                self.hatch_pattern_option.select()
 
             self.white_boxes = tk.BooleanVar()
             self.white_boxes_option = tk.Checkbutton(self.settings_frame, text='White Boxes',variable=self.white_boxes, onvalue=True, offvalue=False,
                                                      command=self.set_white_boxes_option, font=('',10), bg='white', fg='black', anchor=tk.W)
-            self.white_boxes_option.grid(row=2, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            self.white_boxes_option.grid(row=3, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
             if white_boxes:
                 self.white_boxes_option.select()
+
+            self.hatch_pattern = tk.BooleanVar()
+            self.hatch_pattern_option = tk.Checkbutton(self.settings_frame, text='Hatch Pattern',variable=self.hatch_pattern, onvalue=True, offvalue=False,
+                                                       command=self.set_hatch_pattern_option, font=('',10), bg='white', fg='black', anchor=tk.W)
+            self.hatch_pattern_option.grid(row=0, column=1, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            if hatch_pattern:
+                self.hatch_pattern_option.select()
+
 
             self.colorblind_mode = tk.BooleanVar()
             self.colorblind_option = tk.Checkbutton(self.settings_frame, text='Colorblind',variable=self.colorblind_mode, onvalue=True, offvalue=False,
                                                     command=self.set_colorblind_option, font=('',10), bg='white', fg='black', anchor=tk.W)
-            self.colorblind_option.grid(row=3, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            self.colorblind_option.grid(row=1, column=1, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
             if colorblind_mode:
                 self.colorblind_option.select()
 
             self.skip_visualization = tk.BooleanVar()
-            self.skip_visualization_option = tk.Checkbutton(self.settings_frame, text='Skip Visualization',variable=self.skip_visualization, onvalue=True, offvalue=False,
+            self.skip_visualization_option = tk.Checkbutton(self.settings_frame, text='Skip Visual',variable=self.skip_visualization, onvalue=True, offvalue=False,
                                                             command=self.set_skip_visualization_option, font=('',10), bg='white', fg='black', anchor=tk.W)
-            self.skip_visualization_option.grid(row=4, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            self.skip_visualization_option.grid(row=2, column=1, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
             if skip_visualization:
                 self.skip_visualization_option.select()
 
             self.github = tk.PhotoImage(file='images/GitHub-Emblem.png')
             self.github_label = tk.Label(self.settings_frame, image=self.github, bg='black')
-            self.github_label.grid(row=5, column=0, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
+            self.github_label.grid(row=3, column=1, sticky=(tk.N+tk.E+tk.S+tk.W), padx=5, pady=5)
             self.github_label.bind('<Button-1>', self.get_help)
 
             tk.Grid.rowconfigure(self.settings_frame, index=0, weight=1)
             tk.Grid.rowconfigure(self.settings_frame, index=1, weight=1)
             tk.Grid.rowconfigure(self.settings_frame, index=2, weight=1)
             tk.Grid.rowconfigure(self.settings_frame, index=3, weight=1)
-            tk.Grid.rowconfigure(self.settings_frame, index=4, weight=1)
             tk.Grid.columnconfigure(self.settings_frame, index=0, weight=1)
+            tk.Grid.columnconfigure(self.settings_frame, index=1, weight=1)
 
             self.settings_frame.update()
             self.window.minsize((self.settings_frame.winfo_width() + 10), (self.settings_frame.winfo_height() + 10))
@@ -109,13 +130,15 @@ class Settings(tk.Frame):
         self.window.destroy()
         self.window = None
 
-    @staticmethod
-    def get_help(event=None): # pylint: disable=W0613 # argument is required
-        """Opens the link to the official github project of this tool.
+    def set_pre_sampling_option(self):
+        """Sets the 'Pre-Sampling' option from the checkbox."""
+        global pre_sampling # pylint: disable=C0103 # is not a constant
+        pre_sampling = self.pre_sampling.get()
 
-        :param event: Event listener, defaults to None
-        """
-        webbrowser.open('https://github.com/awiegel/PaSyPy')
+    def set_sampling_option(self):
+        """Sets the 'Sampling' option from the checkbox."""
+        global sampling # pylint: disable=C0103 # is not a constant
+        sampling = self.sampling.get()
 
     def set_hyperplane_option(self):
         """Sets the 'hyperplane' option from the checkbox."""
@@ -123,16 +146,16 @@ class Settings(tk.Frame):
         hyperplane = self.hyperplane.get()
         self.parent.start_calculation()
 
-    def set_hatch_pattern_option(self):
-        """Sets the 'hatch pattern' option from the checkbox."""
-        global hatch_pattern # pylint: disable=C0103 # is not a constant
-        hatch_pattern = self.hatch_pattern.get()
-        self.parent.start_calculation()
-
     def set_white_boxes_option(self):
         """Sets the 'white boxes' option from the checkbox."""
         global white_boxes # pylint: disable=C0103 # is not a constant
         white_boxes = self.white_boxes.get()
+        self.parent.start_calculation()
+
+    def set_hatch_pattern_option(self):
+        """Sets the 'hatch pattern' option from the checkbox."""
+        global hatch_pattern # pylint: disable=C0103 # is not a constant
+        hatch_pattern = self.hatch_pattern.get()
         self.parent.start_calculation()
 
     def set_colorblind_option(self):
@@ -158,3 +181,11 @@ class Settings(tk.Frame):
         """Sets the 'skip visualization' option from the checkbox."""
         global skip_visualization # pylint: disable=C0103 # is not a constant
         skip_visualization = self.skip_visualization.get()
+
+    @staticmethod
+    def get_help(event=None): # pylint: disable=W0613 # argument is required
+        """Opens the link to the official github project of this tool.
+
+        :param event: Event listener, defaults to None
+        """
+        webbrowser.open('https://github.com/awiegel/PaSyPy')
